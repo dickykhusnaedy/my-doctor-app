2de4f08cd2ee90f1862abf22791e4a3be6c8337a
Dicky Khusnaedy <dickykhusnaedy98@gmail.com>
2021-01-03 20:53:20 +0700

    add image not found illustration

--- src/assets/illustration/index.js
+++ src/assets/illustration/index.js
@@ -2,5 +2,6 @@ import IL_Logo from './logo.svg';
 import IL_GetStarted from './get-started.png';
 import IL_PhotoNull from './photo-null.png';
 import IL_ImageBG from './hospitals_bg.png';
+import IL_NotFound from './not_found.svg';
 
-export {IL_Logo, IL_GetStarted, IL_PhotoNull, IL_ImageBG};
+export {IL_Logo, IL_GetStarted, IL_PhotoNull, IL_ImageBG, IL_NotFound};
