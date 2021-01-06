0d3bc41fc5816183d3c41a3dc463aaa7c23dcb1f
Dicky Khusnaedy <dickykhusnaedy98@gmail.com>
2021-01-07 00:25:44 +0700

    change image background in get started screen

--- src/assets/illustration/index.js
+++ src/assets/illustration/index.js
@@ -1,5 +1,5 @@
 import IL_Logo from './logo.svg';
-import IL_GetStarted from './get-started.png';
+import IL_GetStarted from './get-started.jpg';
 import IL_PhotoNull from './photo-null.png';
 import IL_ImageBG from './hospitals_bg.png';
 import IL_NotFound from './not_found.svg';
